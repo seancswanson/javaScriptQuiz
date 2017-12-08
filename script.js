4def5d59e8aeaaa336c1ccc7b2ca3115d06c7ff0
seanthaswan <owneruser@Owners-MBP.home>
2017-12-08 06:11:27 -0800

    refactoring and rewriting using const and experimenting with arrow functions, was led down a rabbit hole at beginning and needed to restart

--- script.js
+++ script.js
@@ -1,34 +1,156 @@
 var score = 0;
 
-var allQuestions = [ 
-    { 
-        question: "What race is Frodo?",
-        choices: ["Orc", "Hobbit", "Man", "Elf"],
-        correctAnswerIndex: 1
-    },
-    { 
-        question: "Who is the oldest creature in Middle Earth?",
-        choices: ["Treebeard", "Gandalf", "Elrond", "Aragorn"],
-        correctAnswerIndex: 0
-    },
-    { 
-        question: "What is the name of the Dark Lord's Tower?",
-        choices: ["Amon Amarth", "Dimmu Borgir", "Isengard", "Barad-dur"],
-        correctAnswerIndex: 3
-    },
-    { 
-        question: "How old did Bilbo turning during the celebration in \"The Fellowship Of The Ring?\"",
-        choices: ["Ninety-third", "Twenty-fifty-second", "Eleventy-first", "Seventy-seventh"],
-        correctAnswerIndex: 0
-    },
-    { 
-        question: "What was the name of J.R.R. Tolkien's literary discussion group at Oxford which included C.S. Lewis?",
-        choices: ["Inklings", "Inksters", "Inkmen", "Oxford Commas"],
-        correctAnswerIndex: 0
-    },
+const quizDiv = $(".quiz");
+const resultsDiv = $("#results");
+const next = $("#next");
 
-var q0 = allQuestions[0]["question"];
-var q1 = allQuestions[1]["question"];
-var q2 = allQuestions[2]["question"];
-var q3 = allQuestions[3]["question"];
-var q4 = allQuestions[4]["question"];
+
+var allQuestions = [{
+        question: "What race is Frodo?",
+        choices: {
+            a: "Orc",
+            b: "Hobbit",
+            c: "Man",
+            d: "Elf"
+        },
+        correctAnswerIndex: "b"
+    },
+    {
+        question: "Who is the oldest creature in Middle Earth?",
+        choices: {
+            a: "Treebeard",
+            b: "Gandalf",
+            c: "Elrond",
+            d: "Aragorn"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the name of the Dark Lord's Tower?",
+        choices: {
+            a: "Amon Amarth",
+            b: "Dimmu Borgir",
+            c: "Isengard",
+            d: "Barad-dur"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "How old did Bilbo turning during the celebration in \"The Fellowship Of The Ring?\"",
+        choices: {
+            a: "Ninety-third",
+            b: "Twenty-fifty-second",
+            c: "Eleventy-first",
+            d: "Seventy-seventh"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What was the name of J.R.R. Tolkien's literary discussion group at Oxford which included C.S. Lewis?",
+        choices: {
+            a: "Inklings",
+            b: "Inksters",
+            c: "Inkmen",
+            d: "Oxford Commas"
+        },
+        correctAnswer: "a"
+    }
+];
+
+function generateQandA() {
+    //store output to be generated
+    const output = [];
+
+    allQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            //store list of answer chouces
+            const choices = [];
+
+            // for each answer within each question index
+            for (letter in currentQuestion.choices) {
+
+                //create a radio button
+                 choices.push(
+    `<label>
+      <input type="radio" name="question${questionNumber}" value="${letter}">
+      ${letter} :
+      ${currentQuestion.choices[letter]}
+    </label>`
+  );
+            }
+
+            //add the question and the choices to the output in html
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+  				<div class="answers"> ${choices.join('')} </div>`
+            );
+        }
+    );
+
+    quizDiv.innterHTML = output.join('');
+}
+
+function generateScore() {
+
+}
+
+next.on("click", generateScore);
+
+generateQandA();
+
+
+// var q0 = allQuestions[questionIndex]["question"]["choices"][0];
+// var q1 = allQuestions[questionIndex]["question"]["choices"][0];
+// var q2 = allQuestions[questionIndex]["question"]["choices"][0];
+// var q3 = allQuestions[questionIndex]["question"]["choices"][0];
+
+// function showQuestion() {
+//     // console.log(questionIndex);
+//     // console.log(allQuestions[questionIndex]["question"]);
+//     $(".questionField").hide().fadeIn(1000);
+//     if (questionIndex <= 4) {
+//         $("#questionText").hide().fadeIn(1000).text(allQuestions[questionIndex]["question"]);
+//         $("#next").text("Next question");
+//         console.log(questionIndex);
+//     }
+// }
+
+// function showChoices() {
+//     // console.log(allQuestions[questionIndex]["choices"]);
+//     if (questionIndex <= 4) {
+//     	$("#questionForm").hide().fadeIn(1200);
+//         $("#q0choice").text(allQuestions[questionIndex]["choices"][0]);
+//         $("#q1choice").text(allQuestions[questionIndex]["choices"][1]);
+//         $("#q2choice").text(allQuestions[questionIndex]["choices"][2]);
+//         $("#q3choice").text(allQuestions[questionIndex]["choices"][3]);
+//     }
+// }
+// // function checkQuestion()
+
+// $("#next").on("click", function() {
+//     if (questionIndex <= 4) {
+//     	console.log("score is "+ score);
+//         questionIndex++;
+//         showQuestion();
+//         showChoices();
+//         triggerNext();
+//     }
+// })
+
+// $("input").on("click", function(){
+// 	console.log("logged");
+// 		if(questionIndex===0){
+
+// 		}
+// });
+
+// function triggerNext() {
+// 	if(questionIndex === 4) {
+// 		$("#next").html("Tally score");
+
+// 	}
+// }
+// $(":radio[name='portion_num'][value='1']").attr('checked', 'checked');
+// showQuestion();
+// showChoices();
+// // showQuestion
